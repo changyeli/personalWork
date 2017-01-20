75b99d5717df962b38b7b1c89f32c61ecfc9ad22
palooney <lixx3013@umn.edu>
2017-01-19 22:18:25 -0600

    youtube spider update

--- spider.py
+++ spider.py
@@ -8,25 +8,31 @@ import csv
 ## get title
 def getTitle():
 	for title in soup.findAll('h3', attrs = {'class': 'yt-lockup-title'}):
-		temp = title.find('a').text.strip()
-		return (temp.encode('utf-8'))
+		temp = title.find('a').text.strip().encode('utf-8')
+		head.append(temp)
+	return head[2:]
 
 ## get uploader information
 def getUser():
 	for author in soup.findAll('div', attrs = {'class': 'yt-lockup-byline'}):
-		return (str(author.find('a')['href'][6:]))
+		u1 = str(author.find('a')['href'][6:])
+		uploader.append(u1)
+	return uploader[2:]
 
 ## get number of view
 def getView():
 	for views in soup.findAll('ul', attrs = {'class': 'yt-lockup-meta-info'}):
 		s1 = str(views.findAll('li')).replace(',', '')
-		s1 = re.findall('\d+', s1)[1]
-		return s1
+		s1 = re.findall('\d+', s1)
+		if len(s1) == 2:
+			view.append(s1[1])
+	return view
 
 ## get description of video
 def getDesc():
 	for dess in soup.findAll('div', attrs = {'class': 'yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2'}):
-		return (dess.text.encode('utf-8')[:-11])
+		desc.append(dess.text.encode('utf-8')[:-11])
+	return desc[2:]
 
 ## get next page
 def getNext(count):
@@ -38,48 +44,59 @@ def getNext(count):
 	return pages[0].encode('utf-8')
 
 # na speaker
-
-url = 'https://www.youtube.com/results?q=na+speaker&sp=EgIQAQ%253D%253D'
-count = 1
-datas = []
-
-while (count < 30):
-	count +=1
-	my_page = urllib2.urlopen(url)
-	soup = bs(my_page, 'html.parser')
-	title = getTitle()
-	user = getUser()
-	desc = getDesc()
-	view =getView()
-	url = getNext(count)
-	tup1 = (title, user, desc, view)
-	datas.append(tup1)
-
-# write to .csv file
-with open('info_na.csv', 'wb') as f:
-	fileWriter = csv.writer(f)
-	for row in datas:
-		fileWriter.writerow(row)
-
-# aa speaker
-url = 'https://www.youtube.com/results?q=aa+speaker&sp=EgIQAQ%253D%253D'
+url = 'https://www.youtube.com/results?sp=EgIQAQ%253D%253D&q=na+speaker'
 count = 1
 datas = []
 
 while (count < 29):
-	count +=1
-	my_page = urllib2.urlopen(url)
-	soup = bs(my_page, 'html.parser')
-	title = getTitle()
-	user = getUser()
-	desc = getDesc()
-	view =getView()
-	url = getNext(count)
-	tup1 = (title, user, desc, view)
-	datas.append(tup1)
 
-# write to .csv file
-with open('info_aa.csv', 'wb') as f:
-	fileWriter = csv.writer(f)
-	for row in datas:
-		fileWriter.writerow(row)
+	my_page = urllib2.urlopen(url)
+	soup = bs(my_page, 'lxml') # with order
+	# inilization
+	head = []
+	uploader = []
+	view = []
+	desc = []
+
+	head = getTitle()
+	unloader = getUser()
+	view = getView()
+	desc = getDesc()
+	datas = zip(head, uploader, view, desc) # change to tuple
+
+	count += 1
+	url = getNext(count)
+
+	with open('info_na.csv', 'a') as f: # a to append
+		writer = csv.writer(f)
+		for row in datas:
+			writer.writerow(row)
+
+# aa speaker
+url = 'https://www.youtube.com/results?sp=EgIQAQ%253D%253D&q=aa+speaker'
+count = 1
+datas = []
+
+while (count < 28):
+
+	my_page = urllib2.urlopen(url)
+	soup = bs(my_page, 'lxml') # with order
+	# inilization
+	head = []
+	uploader = []
+	view = []
+	desc = []
+
+	head = getTitle()
+	unloader = getUser()
+	view = getView()
+	desc = getDesc()
+	datas = zip(head, uploader, view, desc) # change to tuple
+
+	count += 1
+	url = getNext(count)
+
+	with open('info_aa.csv', 'a') as f: # a to append
+		writer = csv.writer(f)
+		for row in datas:
+			writer.writerow(row)
